e2ac006aab3ba59fce92958d5a7e476e6218debe
LEE LEE GEAP <lietlee95@gmail.com>
2020-05-21 14:45:58 +0800

    新建3种配置文件 ( base, dev. prod )

---
+++ promote-project/config/webpack.config.prod.js
@@ -0,0 +1,37 @@
+const webpackMerge = require('webpack-merge')
+const baseWebpackConfig = require('./webpack.config.base')
+
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+
+const webpackConfig = webpackMerge(baseWebpackConfig, {
+    mode: 'production',
+    stats: {
+        children: false
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserWebpackPlugin({
+                terserOption: {
+                    warnings: false,
+                    compress: {
+                        warnings: false,
+                        // 是否注释掉console
+                        drop_console: false,
+                        dead_code: true,
+                        drop_debugger: true,
+                    },
+                    output: {
+                        comments: false,
+                        beautify: false,
+                    },
+                    mangle: true,
+                },
+                parallel: true,
+                sourceMap: false,
+            })
+        ],
+    },
+})
+
+module.exports = webpackConfig
